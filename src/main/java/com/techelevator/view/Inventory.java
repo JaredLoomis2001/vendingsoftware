a503e5eb94710c59480e2223868b120dda8b2204
Dustin <slvrshadow89@gmail.com>
2022-12-07 23:29:23 -0500

    Created the Inventory and Product classes.

---
+++ src/main/java/com/techelevator/view/Inventory.java
@@ -0,0 +1,56 @@
+package com.techelevator.view;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/* This class is used to create the vending machine's inventory. It populates a LinkedHashMap with a dataFile by looping
+* through each line in the dataFile and splitting the line into a '|' delimited array. The array then contains 4 indexes:
+*
+* 0 - The item's location in the machine
+* 1 - The item's name
+* 2 - The item's price
+* 3 - The item's type
+*
+*  The item's location is then added to the map as a key using index 0, and a new product is created and added as a value;
+* the product being created with the four indexes listed above.
+*
+*  A method (listProducts) is also provided which loops through each product in the map and calls its printProductInfo()
+* method (provided by the Product class).*/
+
+public class Inventory {
+    // HashMap kept placing the products out of order, so switched to LinkedHashMap
+    // to keep entries in the order in which they are added to the map
+    private Map<String, Product> products = new LinkedHashMap<>();
+
+    public Inventory(File dataFile) {
+        try (Scanner dataInput = new Scanner(dataFile)) {
+            while (dataInput.hasNextLine()) {
+                // Splitting each line into an array
+                String[] items = dataInput.nextLine().split("\\|");
+                // Array indexes will always be 0: Location, 1: Item Name, 2: Price, and 3: Type
+                // Location is used for the key, then a new Product is added as the value
+                products.put(items[0], new Product(items[0], items[1], new BigDecimal(Double.valueOf(items[2])), items[3]));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found at path: " + dataFile.getAbsolutePath());
+        }
+    }
+
+    public Map<String, Product> getProducts() {
+        return products;
+    }
+
+    public void listProducts() {
+        // Runs through each product in the inventory and lists its Location, Name, Price, and current stock
+        System.out.printf("%-4s%-22s%-7s%5s\n","##","Product Name","Price","Qty.");
+        System.out.println("---------------------------------------");
+        for (Map.Entry<String, Product> product : products.entrySet()) {
+            product.getValue().printProductInfo();
+        }
+    }
+}
+
---
+++ src/main/java/com/techelevator/view/Product.java
@@ -0,0 +1,55 @@
+package com.techelevator.view;
+
+import java.math.BigDecimal;
+
+/* This class is used to create Product objects. A constructor is provided to set each product's
+* location within the vending machine, its name, its price, and its type. All products start with 5 items
+* in stock, and a numSold variable to be used as a counter to keep track of how many of that item has been sold.
+*
+*  The printProductInfo method output's a product's location, name, price, and current quantity to the user.*/
+
+public class Product {
+    private String location;
+    private String name;
+    private BigDecimal price;
+    private String type;
+    private int inStock = 5; // Sets starting quantity to 5
+    private int numSold = 0; // Used to keep track of how much of a product has been sold
+
+    public Product(String location, String name, BigDecimal price, String type) {
+        this.location = location;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public void printProductInfo(){
+        // Prints a product's location, name, price, and current stock
+        System.out.printf("%-4s%-22s$%-6.2f%5d\n", location, name, price, inStock);
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getStock() {
+        return inStock;
+    }
+
+    public void setNumSold(int numSold) {
+        this.numSold = numSold;
+    }
+}
+
