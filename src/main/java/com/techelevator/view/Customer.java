c9c6579cf12e65069c7a7c5a50123a723f021db7
Dustin <slvrshadow89@gmail.com>
2022-12-12 16:55:06 -0500

    Fixed bug where feedMoney wasn't adding input to total.

--- src/main/java/com/techelevator/view/Customer.java
+++ src/main/java/com/techelevator/view/Customer.java
@@ -15,14 +15,14 @@ public class Customer {
 
 
     public void feedMoney(BigDecimal amountFed) {
-        this.currentBalance.add(amountFed);
+        this.currentBalance = currentBalance.add(amountFed);
     }
 
     //Update customer balance,subtract the price of item from customer balance
     public void purchaseItem(BigDecimal priceOfItem) {
         //Transaction takes place ,Only if balance is available
         if (priceOfItem.compareTo(this.currentBalance) <= 0)
-            this.currentBalance.subtract(priceOfItem);
+            this.currentBalance =(currentBalance.subtract(priceOfItem));
     }
     //This class is to make things easier and hold all the values for the money in machine
     // for whoever works on Finish Transaction to easier, including a method to change the balance when an item is purchased
